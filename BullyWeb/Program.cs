7efcde453b29fc10afbfd056a67f8ebb57c0156e
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-06-29 22:58:41 +0530

    Category completed

--- BullyWeb/Program.cs
+++ BullyWeb/Program.cs
@@ -1,7 +1,13 @@
+using BullyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option => 
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaulConnection")));
 
 var app = builder.Build();
 
