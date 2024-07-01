70d3e59e1a7bd056458293f5905e2fdda9c11595
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-07-01 21:37:12 +0530

    06 - Product CRUD completed

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -33,6 +33,8 @@ namespace Bully.DataAccess.Data
                     Price = 90,
                     Price50 = 85,
                     Price100 = 80,
+                    CategoryId = 1,
+                    ImageUrl = ""
 
                 },
                 new Product
@@ -46,8 +48,9 @@ namespace Bully.DataAccess.Data
                     Price = 30,
                     Price50 = 25,
                     Price100 = 20,
-      
-                },
+					CategoryId = 1,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 3,
@@ -59,8 +62,9 @@ namespace Bully.DataAccess.Data
                     Price = 50,
                     Price50 = 40,
                     Price100 = 35,
-   
-                },
+					CategoryId = 2,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 4,
@@ -72,8 +76,9 @@ namespace Bully.DataAccess.Data
                     Price = 65,
                     Price50 = 60,
                     Price100 = 55,
- 
-                },
+					CategoryId = 2,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 5,
@@ -85,8 +90,9 @@ namespace Bully.DataAccess.Data
                     Price = 27,
                     Price50 = 25,
                     Price100 = 20,
-
-                },
+					CategoryId = 3,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 6,
@@ -98,8 +104,9 @@ namespace Bully.DataAccess.Data
                     Price = 23,
                     Price50 = 22,
                     Price100 = 20,
-
-                }
+					CategoryId = 3,
+					ImageUrl = ""
+				}
                 );
         }
 
