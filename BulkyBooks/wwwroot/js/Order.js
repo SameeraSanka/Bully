19014db64de9aed0690257734a35e32ce8330f80
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-07-10 22:41:41 +0530

    Order Management

---
+++ BulkyBooks/wwwroot/js/Order.js
@@ -0,0 +1,66 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
+  
+});
+
+function loadDataTable(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": '/Admin/Order/GetAll?status=' + status
+        },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "20%" },
+            { "data": "orderStatus", "width": "7%" },
+            { "data": "orderTotal", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Order/Details?orderId=${data}"
+                            class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> </a>
+                        </div>
+                    `;
+                },
+                "width": "20%"
+            }
+        ],
+        "dom": '<"top"B>rt<"bottom"ip><"clear">', // Custom layout for buttons
+        "buttons": [
+            {
+                extend: 'print',
+                text: 'Print',
+                titleAttr: 'Print Table',
+                className: 'btn btn-primary'
+            },
+
+        ]
+    });
+}
